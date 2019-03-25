4f74d1646a5e8a4b052a618854b8d7910c2c5c3a
xahupt <hu_pt@163.com>
2019-03-25 22:51:50 +0800

    增加了并行流，基本类型流....

---
+++ src/com/percy/Person.java
@@ -0,0 +1,32 @@
+package com.percy;
+
+/**
+ * @author percy
+ * @create 2019-03-25  下午9:31
+ * @descreption:
+ **/
+public class Person {
+    private Integer id;
+    private String name;
+
+    public Person(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/com/percy/streamDemo.java
+++ src/com/percy/streamDemo.java
@@ -6,9 +6,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collector;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 /**
  * @author percy
@@ -73,19 +77,19 @@ public class streamDemo {
              */
             Optional<Object> optional = Optional.of("");
             String result = (String) optional.orElse("");
-            show("String result = (String) optional.orElse(\"\")",Stream.of(result));
+            show("String result = (String) optional.orElse(\"\")", Stream.of(result));
 
-            String result0 = (String) optional.orElseGet(()-> Locale.getDefault().getDisplayName());
-            show("orElseGet",Stream.of(result0));
+            String result0 = (String) optional.orElseGet(() -> Locale.getDefault().getDisplayName());
+            show("orElseGet", Stream.of(result0));
 
-            double x= 0;
-            show("Optional<Double> inverse(Double x) x= 0",Stream.of(inverse(x)));
-            x= 2;
-            show("Optional<Double> inverse(Double x) x= 2",Stream.of(inverse(x)));
+            double x = 0;
+            show("Optional<Double> inverse(Double x) x= 0", Stream.of(inverse(x)));
+            x = 2;
+            show("Optional<Double> inverse(Double x) x= 2", Stream.of(inverse(x)));
 
-            show("2.0",Stream.of(inverse(2.0).flatMap(streamDemo::squareRoot)));
-            show("0.0",Stream.of(inverse(.0).flatMap(streamDemo::squareRoot)));
-            show("-2.0",Stream.of(inverse(-2.0).flatMap(streamDemo::squareRoot)));
+            show("2.0", Stream.of(inverse(2.0).flatMap(streamDemo::squareRoot)));
+            show("0.0", Stream.of(inverse(.0).flatMap(streamDemo::squareRoot)));
+            show("-2.0", Stream.of(inverse(-2.0).flatMap(streamDemo::squareRoot)));
 
             Stream stream2 = words.stream();
             stream2.forEach(System.out::print);
@@ -101,27 +105,88 @@ public class streamDemo {
             TreeSet<String> lists = words.stream().collect(Collectors.toCollection(TreeSet::new));
             System.out.println("针对流中元素收集到另一个目标中，有一个便捷方法collect可用");
             for (String list : lists) {
-                System.out.print(list+" ");
+                System.out.print(list + " ");
             }
             Stream<Locale> localeStream = Stream.of(Locale.getAvailableLocales());
-            Map<String,String> lan = localeStream.collect(Collectors.toMap(Locale::getDisplayLanguage,
-                    locale -> locale.getDisplayLanguage(locale),(existingValue,newValue)->existingValue));
+            Map<String, String> lan = localeStream.collect(Collectors.toMap(Locale::getDisplayLanguage,
+                    locale -> locale.getDisplayLanguage(locale), (existingValue, newValue) -> existingValue));
             for (Map.Entry<String, String> stringStringEntry : lan.entrySet()) {
-                System.out.println(stringStringEntry.getKey() +"="+ stringStringEntry.getValue());
+                System.out.println(stringStringEntry.getKey() + "=" + stringStringEntry.getValue());
             }
 
+            System.out.println("------------------------");
+            Stream<Locale> localeStream1 = Stream.of(Locale.getAvailableLocales());
+            //localeStream1.forEach(System.out::println);
             long count2 = stream.filter(w -> w.length() >= 12).count();
             System.out.println(count2);
+
+
+            Person person0 = new Person(1,"hpt");
+            Person person1 = new Person(2,"yh");
+            Person person2 = new Person(3,"yzw");
+
+            Stream<Person> personStream = Stream.of(person0,person1,person2);
+
+            Map<Integer,String> idToname = personStream.collect(Collectors.toMap(Person::getId,Person::getName));
+            System.out.println("idToname"+idToname.getClass().getName()+idToname);
+            for (Map.Entry<Integer, String> integerStringEntry : idToname.entrySet()) {
+                System.out.println(integerStringEntry.getKey()+"="+integerStringEntry.getValue());
+            }
+            /**
+             * 生成一个Hashmap
+             */
+            personStream = Stream.of(person0,person1,person2);
+            Map<Integer, Person> idToPerson = personStream.collect(Collectors.toMap(Person::getId, Function.identity()));
+            System.out.println("idToPerson:"+idToPerson.getClass().getName()+idToPerson);
+            for (Map.Entry<Integer, Person> integerPersonEntry : idToPerson.entrySet()) {
+                System.out.println(integerPersonEntry.getKey()+"-"+integerPersonEntry.getValue().getName());
+            }
+            /**
+             * 生成一个TreeMap
+             */
+            personStream = Stream.of(person0,person1,person2);
+            idToPerson = personStream.collect(Collectors.toMap(Person::getId,Function.identity(),(existingValue,newValue)->{throw new IllegalStateException();},TreeMap::new));
+            System.out.println("idToPerson:"+idToPerson.getClass().getName()+idToPerson);
+            /**
+             * 基本类型流
+             */
+            IntStream intStream0 = IntStream.generate(()->(int)(Math.random()*100));
+            showIntStream("intStream0",intStream0);
+            IntStream intStream1 = IntStream.range(0,10);
+            showIntStream("intStream1",intStream1);
+            IntStream intStream2 = IntStream.rangeClosed(1,10);
+            showIntStream("intStream2",intStream2);
+            Stream<Integer> integerStream1 = IntStream.rangeClosed(1,10).boxed();
+            IntStream intStream3 = integerStream1.mapToInt(Integer::intValue);
+            showIntStream("intStream3",intStream3);
+            IntStream intStream4 = IntStream.generate(()->(int)(Math.random()*100)).limit(100);
+            System.out.println("数据统计："+intStream4.summaryStatistics());
+            /**
+             * 并行流
+             */
+            int[] shortWords = new int[12];
+            Stream<String> stream3 = words.parallelStream();
+            stream3.forEach(s->{if (s.length()<12) shortWords[s.length()]++;});
+            for (int shortWord : shortWords) {
+                System.out.println(shortWord);
+            }
+
+            Map<Integer,Long> shortWordsCount = words.parallelStream().filter(s->s.length()<12).collect(groupingBy(String::length,counting()));
+            for (Map.Entry<Integer, Long> integerLongEntry : shortWordsCount.entrySet()) {
+                System.out.println(integerLongEntry.getKey()+"-"+integerLongEntry.getValue());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
+
     private static Optional<Double> squareRoot(Double x) {
-        return x<0?Optional.empty():Optional.of(Math.sqrt(x));
+        return x < 0 ? Optional.empty() : Optional.of(Math.sqrt(x));
     }
+
     private static Optional<Double> inverse(Double x) {
-        return x==0?Optional.empty():Optional.of(1/x);
+        return x == 0 ? Optional.empty() : Optional.of(1 / x);
     }
 
     private static <T> void show(String str, Stream<T> stream) {
@@ -134,6 +199,20 @@ public class streamDemo {
         for (int i = 0; i < lists.size(); i++) {
             if (i > 0) System.out.print(",");
             if (i < SIZE) System.out.print(lists.get(i));
+            else System.out.print("...");
+        }
+        System.out.println("");
+    }
+    private static <T> void showIntStream(String str, IntStream stream) {
+        final Integer SIZE = 10;
+        //limit可以用来抽取子流，原流短就短，原流长就长
+        //skip可以跳过前n个元素
+        //concat（a，b）是a元素后跟着的b
+        int[] lists = stream.limit(SIZE + 1).toArray();
+        System.out.print("基本类型流(" + str + "):");
+        for (int i = 0; i < lists.length; i++) {
+            if (i > 0) System.out.print(",");
+            if (i < SIZE) System.out.print(lists[i]);
             else System.out.print("...");
         }
         System.out.println("");
